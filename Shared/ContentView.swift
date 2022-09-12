de3f0a18b8c91f6c9df0f01122f7afcbcaa42f81
Huy Tran <s3878686@rmit.edu.vn>
2022-09-12 10:15:16 +0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Huy Tran Le Quoc on 12/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/YesFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  YesFoodApp.swift
+//  Shared
+//
+//  Created by Huy Tran Le Quoc on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct YesFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
