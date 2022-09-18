66e486a9ffffa7885fd5236a2ca5960ebbd2c2fe
Huy Tran <tranlequochuy2001@gmail.com>
2022-09-18 22:24:54 +0700

    Last edit

--- Shared/View/FoodRow.swift
+++ Shared/View/FoodRow.swift
@@ -1,9 +1,14 @@
-//
-//  FoodRow.swift
-//  YesFood
-//
-//  Created by Huy Tran on 9/18/22.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 3
+  Author: Tran Le Quoc Huy, Dinh Quang Nhat, Ha Minh Duc, Tang Hue Thy
+  ID: s3878686, s3681446, s3878911, s3697323
+  Created  date: 07/07/2022
+  Last modified: 18/09/2022
+  Acknowledgement: RMIT Canvas, Github, Youtube
+*/
 
 import SwiftUI
 
